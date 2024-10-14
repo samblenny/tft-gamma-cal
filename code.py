99e352f8f919aa2a6b8d18084a327f6965032758
sam blenny <68084116+samblenny@users.noreply.github.com>
2024-10-14 04:35:26 +0000

    rework Feather TFT calibrator
    This uses a serial console menu system to control a 7 point gamma
    calibration test pattern on the Feather TFT's built in display. It
    works similar to the html/js desktop browser reference version
    implemented in index.html and main.js.

--- code.py
+++ code.py
@@ -35,120 +35,65 @@ from adafruit_st7789 import ST7789
 
 class GammaCurve:
 
-    # These are state machine state constants for the order of finding
-    # calibration points. To begin with, all we know for sure is that the
-    # display's black is 0x000000 and its white is 0xFFFFFF. By comparing to
-    # a 50% dither of black with white, we can find the color code that will
-    # be perceived as 50% gray (matching the dither pattern's brightness).
-    # Once we know the code for 50% gray, we can find 25% and 75% gray by
-    # comparing to a dithers of black with 50% gray and white with 50% gray.
-    # The same method can find values for 12%, 25%, 37%, 62%, and 87% gray.
-    #
-    GRAY00 = const(0)  # black (zero reference value)
-    GRAY50 = const(1)  # == 50% dither of black with white
-    GRAY25 = const(2)  # == 50% dither of black with GRAY50 value
-    GRAY75 = const(3)  # == 50% dither of GRAY50 value with white
-    GRAY12 = const(4)  # == 50% dither of black with GRAY25 value
-    GRAY87 = const(5)  # == 50% dither of GRAY75 value with white
-    GRAY37 = const(6)  # == 50% dither of GRAY25 value with GRAY50 value
-    GRAY62 = const(7)  # == 50% dither of GRAY50 value with GRAY75 value
-    GRAY99 = const(8)  # white (full-scale reference value)
-
-    # This lookup table lets us get from state machine state constants to the
-    # corresponding indexes of the gamma correction curve.
-    LUT = {
-        GRAY00: 0,  # black
-        GRAY12: 1,
-        GRAY25: 2,
-        GRAY37: 3,
-        GRAY50: 4,  # same brightness as 50% dither of black with white
-        GRAY62: 5,
-        GRAY75: 6,
-        GRAY87: 7,
-        GRAY99: 8,  # white
+    # Preset 7-point calibration curves for some common gamma profiles. Some of
+    # these numbers come from my gamma curve checker web tool (see index.html)
+    BLACK = 0   # index into PRESETS for black
+    WHITE = 1   # index into PRESETS for white
+    PRESETS = {
+        #             black white  1/2  1/4  1/8  1/16  1/32  1/64  1/128
+        "FeatherTFT": (  0,  255,  196, 148, 104,   64,   36,   16,     8),
+        "sRGB-ish":   (  0,  255,  171, 127,  97,   74,   56,   41,    30),
+        "2020's-P3":  (  0,  255,  185, 135,  97,   71,   51,   36,    27),
+        "2010's-TFT": (  0,  255,  184, 131,  94,   68,   50,   36,    28),
     }
 
     def __init__(self):
-        # Initialize with a GRAY50 selected and a linear correction curve
-        self.state = GRAY50
-        self.curve = [0, 7, 15, 23, 31, 39, 47, 55, 63]
-        self.dither_dark  = self.curve[self.LUT[GRAY00]]
-        self.dither_light = self.curve[self.LUT[GRAY99]]
+        # Initialize the gamma curve test pattern
+        self.curve = list(self.PRESETS["FeatherTFT"])
+        self.palette = Palette(len(self.curve))
+        self.bars = len(self.curve) - 2  # omit black, omit white
+        self._update_palette()
+        self.selection = self.WHITE + 1  # start at index for 1/2 brightness
 
-    def set_gray6(self, n):
-        # Change the value of the currently selected 6-bit gray state. Gray
-        # value argument, n, must be in range 1..62 (excludes white and black).
-        if not (0 < n < 63):
-            raise Exception(f"set_gray({n}): gray value is out of range")
-        self.curve[self.LUT[self.state]] = n
+    def load_preset(self, key):
+        # Attempt to load argument key as a gamma curve preset.
+        # Returns True if it worked or False if key is not a preset name
+        if not key in self.PRESETS:
+            return False
+        else:
+            return True
 
-    def update_palette3(self, palette):
-        # Set the colors of the 3-color displayio.Palette argument, palette, to
-        # 24-bit color values corresponding to dither_dark, dither_light, and
-        # the currently selected graypoint, in that order.
+    def _update_palette(self):
+        # Update 24-bit colors in palette to match 8-bit gray values of curve.
+        for (i, gray) in enumerate(self.curve):
+            self.palette[i] = (gray << 16) | (gray << 8) | gray
 
-        # Make an anonymous function to convert 6-bit gray to 24-bit RGB
-        _6to24 = lambda n: ((n << 2) << 16) | ((n << 2) << 8) | (n << 2)
-
-        palette[0] = _6to24(self.dither_dark)
-        palette[1] = _6to24(self.dither_light)
-        palette[2] = _6to24(self.curve[self.LUT[self.state]])
+    def set_gray(self, n):
+        # Change the value of the currently selected 8-bit gray point.
+        if not (0 < n < 255):
+            raise Exception(f"gray value is out of range: {n}")
+        self.curve[self.selection] = n
+        self._update_palette()
 
     def next_graypoint(self):
-        # Advance the state machine to the next graypoint.
-        # Returns: True if the calibration is done, False otherwise
-        _s = self.state
-        _lut   = self.LUT
-        _curve = self.curve
-        if _s == GRAY50:
-            # set dither to black / just-calibrated GRAY50
-            self.dither_dark = _curve[_lut[GRAY00]]
-            self.dither_light = _curve[_lut[GRAY50]]
-            _curve[_lut[GRAY25]] = (self.dither_dark + self.dither_light) // 2
-            self.state = GRAY25
-        elif _s == GRAY25:
-            # set dither to GRAY50 / white
-            self.dither_dark = _curve[_lut[GRAY50]]
-            self.dither_light = _curve[_lut[GRAY99]]
-            _curve[_lut[GRAY75]] = (self.dither_dark + self.dither_light) // 2
-            self.state = GRAY75
-        elif _s == GRAY75:
-            # set dither to black / just-calibrated GRAY25
-            self.dither_dark = _curve[_lut[GRAY00]]
-            self.dither_light = _curve[_lut[GRAY25]]
-            _curve[_lut[GRAY12]] = (self.dither_dark + self.dither_light) // 2
-            self.state = GRAY12
-        elif _s == GRAY12:
-            # set dither to just-calibrated GRAY75 / GRAY99
-            self.dither_dark = _curve[_lut[GRAY75]]
-            self.dither_light = _curve[_lut[GRAY99]]
-            _curve[_lut[GRAY87]] = (self.dither_dark + self.dither_light) // 2
-            self.state = GRAY87
-        elif _s == GRAY87:
-            # set dither to GRAY25 / GRAY50
-            self.dither_dark = _curve[_lut[GRAY25]]
-            self.dither_light = _curve[_lut[GRAY50]]
-            _curve[_lut[GRAY37]] = (self.dither_dark + self.dither_light) // 2
-            self.state = GRAY37
-        elif _s == GRAY37:
-            # set dither to GRAY50 / GRAY75
-            self.dither_dark = _curve[_lut[GRAY50]]
-            self.dither_light = _curve[_lut[GRAY75]]
-            _curve[_lut[GRAY62]] = (self.dither_dark + self.dither_light) // 2
-            self.state = GRAY62
-        elif _s == GRAY62:
-            # set dither back to GRAY00 / GRAY99 (start cycle over)
-            self.dither_dark = _curve[_lut[GRAY00]]
-            self.dither_light = _curve[_lut[GRAY99]]
-            self.state = GRAY50
-            # Tell caller that calibration is done
-            return True
-        # Default: tell caller calibration is not done yet
-        return False
+        # Select next graypoint, skipping first (white) and last (black).
+        n = self.selection - 2
+        self.selection = self.WHITE + 1 + ((n + 1) % self.bars)
+
+    def prev_graypoint(self):
+        # Select previous graypoint, skipping first (white) and last (black).
+        n = self.selection - 2
+        self.selection = self.WHITE + 1 + ((n - 1) % self.bars)
 
     def __str__(self):
-        # Return printable string for this gamma curve
-        return str(self.curve)
+        # Return printable string for this gamma curve (omit white and black).
+        s = self.selection
+        curve = self.curve
+        # build string with stars around current selected graypoint
+        left  = ''.join(['%03d   ' % n for n in curve[self.WHITE+1:s]])
+        sel   = '%03d<' % curve[s]
+        right = ''.join(['  %03d ' % n for n in curve[s+1:]])
+        return '%s%s%s' % (left, sel, right)
 
 
 def main():
@@ -162,62 +107,64 @@ def main():
     display = board.DISPLAY
     gc.collect()
 
+    # Make a gamma curve object which includes a .palette property which we
+    # will need for making a TileGrid later
+    gamma_curve = GammaCurve()
+
     # Make a bitmap for the gamma calibration pattern
     #
-    # The calibration pattern has a thick border of a 50% dither pattern around
-    # a central square of a solid color. The idea is to determine correction
-    # coefficients to match perceived brightness values of the solid square
-    # compared to the dithered border.
+    # The pattern has 7 vertical bars. The top half of each bar is made from a
+    # checkerboard dither pattern. The bottom half of each bar is a solid
+    # color. Because human eyes are tricky, if the gamma curve values are
+    # adjusted right, the top and bottom half of each bar will appear to be the
+    # same hue and value.
     #
-    # For example, suppose the palette (pal) is
-    #   pal[0] = 0x000000   # black
-    #   pal[1] = 0xFFFFFF   # white
-    #   pal[2] = 0x808080   # theoretically 50% gray, but likely more or less
+    # The bitmap's pattern of indexed colors for the dithered and solid grays
+    # looks kinda like this, but scaled up:
     #
-    # with a calibration pattern of
-    #   010101010101
-    #   101010101010
-    #   010122220101
-    #   101022221010
-    #   010101010101
-    #   101010101010
+    #    0101 0202 0303 0404 0505 0606 0707  \   upper half of each bars is
+    #    1010 2020 3030 4040 5050 6060 7070   }  dithered in a checkerboard
+    #    0101 0202 0303 0404 0505 0606 0707  /   pattern
     #
-    # The calibration goal would be to adjust pal[2] until it matches the
-    # perceived brightness of a 50% dither between pal[0] and pal[1].
-
-    # 1. Compute boundary coordinates for the outer dither pattern ring and
-    #    the inner solid color box
-    PAD    = const(32)
-    LEFT   = const((TFT_W * 1) // 5)
-    RIGHT  = const((TFT_W * 4) // 5)
-    TOP    = const(((TFT_H - PAD) * 2 ) // 7)
-    BOTTOM = const(((TFT_H - PAD) * 5 ) // 7)
-
-    # 2. Make the bitmap
-    bmp = Bitmap(TFT_W, TFT_H - PAD, 3)
-
-    # 3. Fill bitmap with the outer dither pattern ring and inner solid box
+    #    2222 3333 4444 5555 6666 7777 8888  \
+    #    2222 3333 4444 5555 6666 7777 8888   }  lower halves are solid gray
+    #    2222 3333 4444 5555 6666 7777 8888  /
+    #     ^    ^    ^    ^    ^    ^    ^
+    #    1/2  1/4  1/8  1/16 1/32 1/64 1/128
+    #
+    #  Note that palette index 0 is white, 1..6 are grays, and 7 is black.
+    #
+    PAD = const(30)
+    width = TFT_W
+    height = TFT_H - PAD
+    bmp = Bitmap(width, height, len(gamma_curve.palette))
+    middle = height // 2
+    number_of_bars = gamma_curve.bars
+    px_per_bar = TFT_W // number_of_bars
     for y in range(bmp.height):
-        for x in range(bmp.width):
-            if TOP < y < BOTTOM and LEFT < x < RIGHT:
-                bmp[x, y] = 2
+        for x in range(bmp.width // number_of_bars * number_of_bars):
+            # Select palette colors for the dither pattern based on the current
+            # x coordinate. Palette[0] is white, Palette[1] is the 1/2
+            # brightness gray value, Palette[2] is 1/4 brightness, etc.
+            which_vertical_bar = x // px_per_bar
+            # Dark dither color is always black
+            dark = gamma_curve.BLACK
+            # Light dither color is white for the leftmost bar. Then, for each
+            # bar to the right, it takes the value of the solid gray color from
+            # the previous (one step lighter/to-the-left) gray bar.
+            light = gamma_curve.WHITE + which_vertical_bar
+            solid = light + 1
+            if y < middle:
+                dither_light = (x ^ y) & 1
+                bmp[x, y] = light if dither_light else dark
             else:
-                bmp[x, y] = (x ^ y) & 1  # 50% dither pattern between 0 and 1
-
-
-    # Make a 3 color palette of black, white, and 50% gray (which will probably
-    # look like something other than 50% gray because the gamma curve is wrong)
-    pal = Palette(3)
-
-    # Make a gamma correction curve manager and set the initial palette
-    gamma = GammaCurve()
-    gamma.update_palette3(pal)
+                bmp[x, y] = solid
 
     # Combine the bitmap and palette into a TileGrid
-    test_pattern = TileGrid(bmp, pixel_shader=pal, x=0, y=PAD)
+    test_pattern = TileGrid(bmp, pixel_shader=gamma_curve.palette, x=0, y=PAD)
 
     # Make a text label for status messages
-    status = bitmap_label.Label(FONT, text="", color=0xFF0000, scale=2)
+    status = bitmap_label.Label(FONT, text="", color=0xFF0000, scale=1)
     status.anchor_point = (0, 0)
     status.anchored_position = (0, 0)
 
@@ -227,67 +174,53 @@ def main():
     grp.append(status)
     display.root_group = grp
 
-    # Cache frequently used callables to save time on dictionary name lookups
-    # (this is a standard MicroPython performance boosting trick)
-    _collect = gc.collect
-    _sleep = time.sleep
-
     # MAIN EVENT LOOP
-    error_msg = 'please enter a number from 1 to 62, or "next"'
-    palette_format = 'dither:%2d/%2d box:%2d'
-    print("\nBeginning gamma curve calibration...\n")
-    print("- Your goal is to enter a 6-bit grayscale value (1..62) for the")
-    print("  center box to make its brightness match the brightness of the")
-    print("  dithered ring surrounding the box.\n")
-    print("- The 3 numbers on each prompt line are gray values for the dither")
-    print("  ('dither: <dark>/<light>') and the center box ('box: <box>').\n")
-    print('- When the box and ring brightness match, enter "next" to advance')
-    print("  to the next gray point on the calibration curve.\n\n")
+    help_message = """
+============================
+=== TFT Gamma Calibrator ===
+============================
+
+Your goal is to adjust the brightness value of the bottom half of each gray bar
+shown on the Feather TFT's display to match the top half. Begin with the
+leftmost bar, which is a 50% dither of 0x000000 (black) and 0xFFFFFF (white).
+
+Commands:
+
+  1..254   set selected graypoint to an 8-bit integer gray value
+       n   select Next graypoint
+       p   select Previous graypoint
+       ?   show this help message
+"""
+    print(help_message)
+    waiting_on_serial_input = True
     while True:
-        (dark, light, mid) = [gray24_to_6(c) for c in (pal[0], pal[1], pal[2])]
-        grays_status = palette_format % (dark, light, mid)
-        status.text = grays_status
-        _collect()
-        _sleep(0.1)
-        # Prompt for a new grayscale value for the center box. This uses 6-bit
-        # values to make the data entry easier and to use less space on the
-        # TFT display's status line
-        ans = input('%s  set box to: ' % grays_status)
+        curve_txt = str(gamma_curve)
+        if waiting_on_serial_input:
+            status.text = 'CHECK SERIAL CONSOLE\n%s' % curve_txt
+        else:
+            status.text = '\n%s' % curve_txt
+        time.sleep(0.1)
+        # Prompt for a new grayscale value
+        ans = input('[%s]: ' % curve_txt)
+        waiting_on_serial_input = False
         try:
             n = int(ans)
-            if 1 <= n <= 62:
-                # If answer was an in-range grayscale value, change box color
-                gamma.set_gray6(n)
-                gamma.update_palette3(pal)
+            if 0 < n < 255:
+                # If answer was an in-range grayscale value, change the value
+                gamma_curve.set_gray(n)
             else:
-                print(error_msg)
+                print("gray value out of range")
         except ValueError as e:
-            if ans == 'next':
-                # If answer was "next", advance to the next graypoint
-                done = gamma.next_graypoint()
-                gamma.update_palette3(pal)
-                if done:
-                    print("===================================")
-                    print("Calibration is done!")
-                    print(gamma)
-                    print("===================================")
+            if ans == 'n':                    # "n" for next curve point
+                gamma_curve.next_graypoint()
+            elif ans == 'p':                  # "p" for previous curve point
+                gamma_curve.prev_graypoint()
+            elif ans == '?':                  # "?" for help
+                print(help_message)
+            elif ans == '':                   # ignore ""
+                pass
             else:
-                print(error_msg)
-
-
-def gray6_to_24(n):
-    # Return the 6-bit grayscale argument n as a 24-bit RGB color
-    if not (0 <= n <= 63):
-        raise Exception(f"6-bit grayscale value out of range: {n}")
-    n <<= 2
-    return (n << 16) | (n << 8) | n
-
-
-def gray24_to_6(c):
-    # Return the 6-bit grayscale value for the 24-bit color argument c
-    if not (0 <= c <= 0xffffff):
-        raise Exception(f"24-bit color value out of range: {n}")
-    return (c & 0xff) >> 2
+                print('eh, what? (for help, try "?")')
 
 
 main()
